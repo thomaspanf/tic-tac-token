627cb077bee4f65a387e8c783be511e7dd804548
Thomas Pan <thomaspanf@gmail.com>
2022-09-24 12:26:50 -0400

    1.5.5

--- src/FizzBuzz.sol
+++ src/FizzBuzz.sol
@@ -3,9 +3,12 @@ pragma solidity 0.8.10;
 
 contract FizzBuzz {
     function fizzbuzz(uint256 n) public pure returns (string memory) {
-if (n == 5) {
+        if (n == 5) {
             return "buzz";
         }
-        return "fizz";
+        if (n == 3) {
+            return "fizz"; 
+        }
+        return string(n);
     }
 }
